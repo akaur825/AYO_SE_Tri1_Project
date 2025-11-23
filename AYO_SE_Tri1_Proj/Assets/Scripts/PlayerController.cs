f571fe9b3d2e0e83c7bf69bdaf7cef4f70df2c5b
yastri27 <yastri27@bergen.org>
2025-11-22 19:51:27 -0500

    added the sprite for the player, and configured basic movement animations and keys

---
+++ AYO_SE_Tri1_Proj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Vector2 input;
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
+        playerAnimator.SetFloat("Horizontal", input.x);
+        playerAnimator.SetFloat("Vertical", input.y);
+        playerAnimator.SetFloat("Speed", input.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.linearVelocity = input * speed;
+        Debug.Log(playerRb.linearVelocity);
+    }
+}
