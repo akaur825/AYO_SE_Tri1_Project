84e989b0c06821b30640fdb4a325677b83f8f422
avnkau27 <avnkau27@bergen.org>
2025-11-27 22:09:43 -0500

    The enemies spawn randomly between 10-15 seconds.

--- AYO_SE_Tri1_Proj/Assets/Scripts/EnemyController.cs
+++ AYO_SE_Tri1_Proj/Assets/Scripts/EnemyController.cs
@@ -9,22 +9,50 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
+    [SerializeField] 
+    private GameObject enemy;
+
     [SerializeField]
     private EnemyMovement enemyMovement;
 
     [SerializeField]
     private float timer;
 
+    [SerializeField]
+    private float minSpawnTime;
+
+    [SerializeField]
+    private float maxSpawnTime;
+
+    public bool gameEnded = false;
+    public Transform spawnPoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       StartCoroutine(DestroyAfterTimer());
+        StartCoroutine(DestroyAfterTimer());
+        StartCoroutine(RandomlySpawnEnemies());
     }
 
     IEnumerator DestroyAfterTimer()
-    { 
+    {
         yield return new WaitForSeconds(timer);
         Destroy(gameObject);
+    }
+
+    IEnumerator RandomlySpawnEnemies()
+    {
+        while (gameEnded != true)
+        {
+            float randomSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(randomSpawnTime);
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+            Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
     }
 
     // Update is called once per frame
