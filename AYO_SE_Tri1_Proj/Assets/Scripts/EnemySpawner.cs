4d30b66afac249ca3baba36a1891d8d482ee7d0f
avnkau27 <avnkau27@bergen.org>
2025-11-30 15:38:00 -0500

    Fixed the enemy spawning bug. Also, separated the EnemyController script into two.

---
+++ AYO_SE_Tri1_Proj/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    private float minSpawnTime;
+
+    [SerializeField]
+    private float maxSpawnTime;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    public bool gameEnded = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(RandomlySpawnEnemies());
+    }
+    IEnumerator RandomlySpawnEnemies()
+    {
+        while (gameEnded != true)
+        {
+            float randomSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(randomSpawnTime);
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
