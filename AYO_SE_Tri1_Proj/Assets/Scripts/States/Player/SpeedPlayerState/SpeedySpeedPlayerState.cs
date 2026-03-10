a9aba7731bba6a5b251d3324e4313a935c0d4e2d
SleepyOliveTrees <olivia.j.smith@outlook.com>
2026-03-09 22:05:42 -0400

    SpeedPlayerStates should be completed!

--- AYO_SE_Tri1_Proj/Assets/Scripts/States/Player/SpeedPlayerState/SpeedySpeedPlayerState.cs
+++ AYO_SE_Tri1_Proj/Assets/Scripts/States/Player/SpeedPlayerState/SpeedySpeedPlayerState.cs
@@ -1,31 +1,41 @@
-using Codice.CM.Common;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedySpeedPlayerState : SpeedPlayerState
 {
+    private bool stillWaiting;
+    private float effecCooldown;
     public SpeedySpeedPlayerState(PlayerController p)
     {
         player = p;
         speed = 7.0f;
-    }
+        stillWaiting = true;
+        effecCooldown = Random.Range(5f, 8f);
+        player.StartCoroutine(EffectCooldown(effecCooldown));
 
+    }
     public override State Act()
     {
-        throw new System.NotImplementedException();
+        if (stillWaiting)
+        {
+            return this;
+        }
+        else
+        {
+            player.advanceSpeedState(new NormalSpeedPlayerState(player), null);
+            return new NormalSpeedPlayerState(player);
+        }
     }
 
     public override SpeedPlayerState advanceState(Collider2D other)
     {
-        throw new System.NotImplementedException();
+        return this;
     }
 
-    /*
-         IEnumerator EffectCooldown()
+    private IEnumerator EffectCooldown(float effectCooldown)
     {
-        yield return new WaitForSeconds(7);
-        hasEffect = false;
-        speed = speedNormal;
-        SetState(new NormalSpeedPlayerState(this));
+        yield return new WaitForSeconds(effectCooldown);
+        stillWaiting = false;
     }
-     */
 }
